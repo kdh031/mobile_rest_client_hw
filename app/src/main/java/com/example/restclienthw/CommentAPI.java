93811a52e2b0bdb225be4acb4ad46d4167ebd23b
kdh031 <kaliadhall@gmail.com>
2020-12-06 18:57:25 -0600

    Initial commit

---
+++ app/src/main/java/com/example/restclienthw/CommentAPI.java
@@ -0,0 +1,26 @@
+package com.example.restclienthw;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface CommentAPI {
+    @GET("comments/")
+    Call<List<Comment>> loadAllComments();
+
+    @GET("comments/")
+    Call<List<Comment>> loadCommentByPostId(@Query("postId") int postId);
+
+
+    @POST("comments/")
+    @FormUrlEncoded
+    Call<Comment> addCommentToPost(@Field("postId") int postId,
+                                   @Field("name") String name,
+                                   @Field("email") String email,
+                                   @Field("body") String body);
+}
---
+++ app/src/main/java/com/example/restclienthw/PostAPI.java
@@ -0,0 +1,12 @@
+package com.example.restclienthw;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PostAPI {
+    @GET("posts/")
+    Call<List<Post>> loadPosts();
+}
